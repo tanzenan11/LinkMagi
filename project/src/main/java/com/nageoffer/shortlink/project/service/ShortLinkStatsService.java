f9c5002210c1e841601090b5809efab8a8edd2af
itcast <hello@itcast.cn>
2024-08-14 10:44:25 +0800

    短链接监控之分组短链接监控数据

--- project/src/main/java/com/nageoffer/shortlink/project/service/ShortLinkStatsService.java
+++ project/src/main/java/com/nageoffer/shortlink/project/service/ShortLinkStatsService.java
@@ -1,6 +1,7 @@
 package com.nageoffer.shortlink.project.service;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.nageoffer.shortlink.project.dto.req.ShortLinkGroupStatsReqDTO;
 import com.nageoffer.shortlink.project.dto.req.ShortLinkStatsAccessRecordReqDTO;
 import com.nageoffer.shortlink.project.dto.req.ShortLinkStatsReqDTO;
 import com.nageoffer.shortlink.project.dto.resp.ShortLinkStatsAccessRecordRespDTO;
@@ -26,4 +27,12 @@ public interface ShortLinkStatsService {
      * @return 访问记录监控数据
      */
     IPage<ShortLinkStatsAccessRecordRespDTO> shortLinkStatsAccessRecord(ShortLinkStatsAccessRecordReqDTO requestParam);
+
+    /**
+     * 获取分组短链接监控数据
+     *
+     * @param requestParam 获取分组短链接监控数据入参
+     * @return 分组短链接监控数据
+     */
+    ShortLinkStatsRespDTO groupShortLinkStats(ShortLinkGroupStatsReqDTO requestParam);
 }
