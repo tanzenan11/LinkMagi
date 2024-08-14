dd322550777048b150d7e7fd74f13c4cd1e4c2ab
itcast <hello@itcast.cn>
2024-08-14 22:57:02 +0800

    优化短链接后管用户返回数据代码

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -94,17 +94,11 @@ public class UserTransmitFilter implements Filter {
 
     /*返回客户端数据*/
     private void returnJson(HttpServletResponse response, String json) throws Exception {
-        PrintWriter writer = null;
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=utf-8");
-        try {
-            writer = response.getWriter();
+        // 省略了关闭io流的操作
+        try (PrintWriter writer = response.getWriter()) {
             writer.print(json);
-
-        } catch (IOException e) {
-        } finally {
-            if (writer != null)
-                writer.close();
         }
     }
 }
