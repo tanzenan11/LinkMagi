5196cbc9e332a830e5d21d7780c47ba6f90ab96c
itcast <hello@itcast.cn>
2024-08-15 22:51:28 +0800

    优化短链接相关缓存 Key 规范

--- project/src/main/java/com/nageoffer/shortlink/project/common/constant/RedisKeyConstant.java
+++ project/src/main/java/com/nageoffer/shortlink/project/common/constant/RedisKeyConstant.java
@@ -46,4 +46,14 @@ public class RedisKeyConstant {
      * 短链接延迟队列消费统计 Key
      */
     public static final String DELAY_QUEUE_STATS_KEY = "short-link_delay-queue:stats";
+
+    /**
+     * 短链接统计判断是否新用户缓存标识
+     */
+    public static final String SHORT_LINK_STATS_UV_KEY = "short-link:stats:uv:";
+
+    /**
+     * 短链接统计判断是否新 IP 缓存标识
+     */
+    public static final String SHORT_LINK_STATS_UIP_KEY = "short-link:stats:uip:";
 }
