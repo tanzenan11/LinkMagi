aa129d9945df3eac5cc7c71172d029c334cad19f
itcast <hello@itcast.cn>
2024-08-15 19:02:39 +0800

    rabbit mq消息幂等类注释

--- project/src/main/java/com/nageoffer/shortlink/project/config/RabbitMqIdempotentConfiguration.java
+++ project/src/main/java/com/nageoffer/shortlink/project/config/RabbitMqIdempotentConfiguration.java
@@ -7,12 +7,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * SpringAMQP的MessageConverter唯一消息ID
+ * SpringAMQP的MessageConverter生成唯一消息ID保证幂等性
  */
 @Configuration
 @ConditionalOnProperty(prefix = "message-queue", name = "select", havingValue = "mq")
 public class RabbitMqIdempotentConfiguration {
 
+    /**
+     * 生成唯一消息id
+     * @return
+     */
     @Bean
     public MessageConverter messageConverter() {
         // 创建 Jackson2JsonMessageConverter 实例
