c05664ebe7071ddfc03ec3895c21c632d2728136
itcast <hello@itcast.cn>
2024-08-16 10:32:06 +0800

    修改相似原始链接短链接大量并发场景下生成冲突问题

--- project/src/main/java/com/nageoffer/shortlink/project/service/Impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/nageoffer/shortlink/project/service/Impl/ShortLinkServiceImpl.java
@@ -566,8 +566,8 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                 throw new ServiceException("短链接频繁生成，请稍后再试");
             }
             String originUrl = requestParam.getOriginUrl();
-            // 加入当前毫秒值再做哈希是为了防止哈希冲突，这个值越长哈希冲突的可能性就越小
-            originUrl += System.currentTimeMillis();
+            // 加入随机UUID再做哈希是为了防止哈希冲突，这个值越长哈希冲突的可能性就越小
+            originUrl += UUID.randomUUID().toString().replace("-", "");
             shortUri = HashUtil.hashToBase62(originUrl);
             if (!shortUriCreateCachePenetrationBloomFilter.contains(createShortLinkDefaultDomain + "/" + shortUri)) {
                 // 布隆过滤器判断不存在代表没有该数据直接返回
